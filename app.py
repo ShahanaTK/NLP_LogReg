ee495a1577ff22c17c09b3bda0c71aeb20f97e10
ShahanaTK <shiyya2002@gmail.com>
2024-04-30 16:13:33 +0530

    My Sentiment Analysis code

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+
+model=joblib.load("sentiment-model.pkl")
+
+senti_labels={1:"Positive",0:"Negative"}
+
+st.title("Sentiment Analysis")
+user_input=st.text_area("Enter your text here:")
+if st.button("Predict"):
+    predict_sen=model.predict([user_input])[0]
+    print(predict_sen)
+    pred_senti_lab=senti_labels[predict_sen]
+    st.info(f"Predicted Sentiment: {pred_senti_lab}")
